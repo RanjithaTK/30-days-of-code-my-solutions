f025c95dbf93a1ef141de0472f43abda2f3ddc91
im ranjitha <ranjithatk101@gmail.com>
2022-06-26 23:19:03 +0530

    changes

--- day9.js
+++ day9.js
@@ -1,18 +1,26 @@
-function factorial(n){
-   
-    var fact =1 ;       
-   for(let i = n;i>0;i--){     // Get factorial of the number
-       fact = fact * i
-   }
-   return fact;
-    
+function factorial(n) {
+  var fact = 1
+  for (let i = n; i > 0; i--) {
+    // Get factorial of the number
+    fact = fact * i
+  }
+  return fact
 }
 // function factorial(n){
-   
-//     var fact =1 ;       
+
+//     var fact =1 ;
 //    for(let i = n;i>0;i--){     // Get factorial of the number
 //        fact = fact * i
 //    }
 //    return fact;
-    
+
+// }
+// function factorial(n){
+
+//     var fact =1 ;
+//    for(let i = n;i>0;i--){     // Get factorial of the number
+//        fact = fact * i
+//    }
+//    return fact;
+
 // }
