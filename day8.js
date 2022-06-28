b0a4774549da6cdfbd4c0a52053221c6d56b0085
im ranjitha <ranjithatk101@gmail.com>
2022-06-28 17:25:24 +0530

    changes

--- day8.js
+++ day8.js
@@ -1,19 +1,36 @@
 function processData(input) {
-    
-    input = input.split('\n');
-    var phoneBook = [];
-  
-    for (i = 1; i <= parseInt(input[0]); i++) {
-      var contactArray = input[i].split(' ');
-      phoneBook[contactArray[0]] = contactArray[1];
+  input = input.split("\n")
+  var phoneBook = []
+
+  for (i = 1; i <= parseInt(input[0]); i++) {
+    var contactArray = input[i].split(" ")
+    phoneBook[contactArray[0]] = contactArray[1]
+  }
+
+  for (i = parseInt(input[0]) + 1; i < input.length; i++) {
+    if (phoneBook[input[i]]) {
+      console.log(input[i] + "=" + phoneBook[input[i]])
+    } else {
+      console.log("Not found")
     }
-  
-    for (i = (parseInt(input[0]) + 1); i < input.length; i++){
-      if (phoneBook[input[i]]) {
-        console.log(input[i] + '=' + phoneBook[input[i]]);
-      } else {
-        console.log('Not found');
-      }
-    }
-  
-  } 
+  }
+}
+// function processData(input) {
+
+//     input = input.split('\n');
+//     var phoneBook = [];
+
+//     for (i = 1; i <= parseInt(input[0]); i++) {
+//       var contactArray = input[i].split(' ');
+//       phoneBook[contactArray[0]] = contactArray[1];
+//     }
+
+//     for (i = (parseInt(input[0]) + 1); i < input.length; i++){
+//       if (phoneBook[input[i]]) {
+//         console.log(input[i] + '=' + phoneBook[input[i]]);
+//       } else {
+//         console.log('Not found');
+//       }
+//     }
+
+//   }
